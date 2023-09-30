fb13648c8e0ab6fd41d743ae8d57471cf6999546
Furkan Emin Can <femincanx@gmail.com>
2023-09-30 08:54:11 +0300

    feat: change project name and logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line import/no-unresolved, import/no-absolute-path
-import StarStruck from '/star-struck.png';
+import ForkKnifePlate from '/fork-knife-plate.svg';
 
 export const Logo = () => (
   <div className='flex place-items-center'>
     <img
-      src={StarStruck}
-      alt='Star struck emoji'
+      src={ForkKnifePlate}
+      alt='Fork, knife, and plate emoji'
       className='h-24 w-24 sm:h-28 sm:w-28 md:h-32 md:w-32'
     />
   </div>
