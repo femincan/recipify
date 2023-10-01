71a1c2b709995f33daed952774754218793fc8c3
Furkan Emin Can <femincanx@gmail.com>
2023-10-01 15:37:52 +0300

    feat: add useMeal hook

---
+++ src/hooks/useMeal.ts
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import { MealData } from '@src/types';
+
+const getMealsByQuery = async (searchQuery: string, signal: AbortSignal) => {
+  const result = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`,
+    { signal },
+  );
+
+  if (!result.ok) throw new Error('An error occured during the request!');
+
+  const data = await result.json();
+
+  return data as MealData;
+};
+
+export const useMeal = (searchQuery: string) => {
+  const queryResult = useQuery({
+    queryKey: ['meals', searchQuery],
+    queryFn: ({ signal }) => getMealsByQuery(searchQuery, signal),
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    staleTime: Infinity,
+  });
+
+  return { ...queryResult };
+};
