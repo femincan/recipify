1198bbeb149d835fa1d2de74cc33bcfa7bf5d1da
Furkan Emin Can <femincanx@gmail.com>
2023-11-09 14:43:32 +0300

    feat: add normalizeMealData function

---
+++ src/lib/normalizeMealData.ts
@@ -0,0 +1,34 @@
+import { Ingredients, MealApiData, MealData } from '@src/index.types';
+
+const normalizeIngredients = (mealApiData: MealApiData): Ingredients => {
+  const ingredients: Ingredients = [];
+
+  for (let i = 1; i <= 20; i += 1) {
+    type MealApiDataKey = keyof MealApiData;
+
+    const ingredient = mealApiData[`strIngredient${i}` as MealApiDataKey];
+    const measure = mealApiData[`strMeasure${i}` as MealApiDataKey];
+
+    if (!ingredient || !measure) {
+      break;
+    }
+
+    ingredients.push({ [ingredient]: measure });
+  }
+
+  return ingredients;
+};
+
+export const normalizeMealData = (mealApiData: MealApiData): MealData => {
+  const ingredients = normalizeIngredients(mealApiData);
+
+  return {
+    id: mealApiData.idMeal,
+    name: mealApiData.strMeal,
+    category: mealApiData.strCategory,
+    area: mealApiData.strArea,
+    instructions: mealApiData.strInstructions,
+    thumb: mealApiData.strMealThumb,
+    ingredients,
+  };
+};
