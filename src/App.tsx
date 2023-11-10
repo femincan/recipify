8e887ac16ae52e1bcc0117e56d2262bb037c47cd
Furkan Emin Can <femincanx@gmail.com>
2023-11-10 17:43:11 +0300

    feat: add App component content

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
-const App = () => <div>App</div>;
+import { Logo } from './components/Logo';
+import { MealSearch } from './components/MealSearch';
 
-export { App };
+export const App = () => (
+  <div className='container mx-auto flex h-full w-full flex-col items-center justify-start gap-10 px-5 py-10'>
+    <Logo />
+    <MealSearch />
+  </div>
+);
